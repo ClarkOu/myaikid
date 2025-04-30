2cdb7a0da685e52a93a8615566420d2002e5a12a
ClarkOu <158656996+ClarkOu@users.noreply.github.com>
2025-04-29 21:21:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
             
             // 根据名称跳转到不同页面
             if (name === '全球新聞 DEMO') {
-                window.location.href = 'https://clarkou.github.io/crazynews/';
+                window.open(URL, '_blank') = 'https://clarkou.github.io/crazynews/';
             } else if (name === 'ALVAZONE 小助理 DEMO') {
                 window.location.href = 'alvazone.html';
             }
