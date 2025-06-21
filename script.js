faccad39285e1bba51b31be56bd37ae47a49fdf0
ClarkOu <158656996+ClarkOu@users.noreply.github.com>
2025-06-21 15:16:23 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -18,6 +18,22 @@ document.addEventListener('DOMContentLoaded', function() {
                 window.location.href = 'globalnews.html';
             } else if (name === 'ALVAZONE 小助理 DEMO') {
                 window.location.href = 'alvazone.html';
+            } else if (name === '1P') {
+                window.location.href = '1p.html';
+            } else if (name === '2M') {
+                window.location.href = '2m.html';
+            } else if (name === '3M') {
+                window.location.href = '3m.html';
+            } else if (name === '4M') {
+                window.location.href = '4m.html';
+            } else if (name === '5M') {
+                window.location.href = '5m.html';
+            } else if (name === '6M') {
+                window.location.href = '6m.html';
+            } else if (name === '4PC') {
+                window.location.href = '4pc.html';
+            } else if (name === '4PB') {
+                window.location.href = '4pb.html';
             }
             // 其他AI角色可以根据需要添加跳转逻辑
         });
